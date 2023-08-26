82eb68a7519ab24f206d57483a825bd2c1ae616b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-26 18:24:21 +0300

    IoC Container XmlApplicationContext

--- src/main/java/ru/philimonov/springcourse/ApplicationRunner.java
+++ src/main/java/ru/philimonov/springcourse/ApplicationRunner.java
@@ -1,28 +1,14 @@
 package ru.philimonov.springcourse;
 
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.philimonov.springcourse.database.pool.ConnectionPool;
-import ru.philimonov.springcourse.database.repository.CompanyRepository;
-import ru.philimonov.springcourse.database.repository.UserRepository;
-import ru.philimonov.springcourse.ioc.Container;
-import ru.philimonov.springcourse.service.UserService;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean(ConnectionPool.class));
 
-        var container = new Container();
-//
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//
-//        UserService userService = new UserService(userRepository, companyRepository);
-//
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
-
-        //// TODO: 26.08.2023 userService
     }
 }
