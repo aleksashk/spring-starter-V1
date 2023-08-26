e07a50b72a10c2c9b81b3cf34ce4bc99877175f9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-26 17:54:55 +0300

    IoC  -> DI

--- src/main/java/ru/philimonov/springcourse/ApplicationRunner.java
+++ src/main/java/ru/philimonov/springcourse/ApplicationRunner.java
@@ -3,15 +3,25 @@ package ru.philimonov.springcourse;
 import ru.philimonov.springcourse.database.pool.ConnectionPool;
 import ru.philimonov.springcourse.database.repository.CompanyRepository;
 import ru.philimonov.springcourse.database.repository.UserRepository;
+import ru.philimonov.springcourse.ioc.Container;
 import ru.philimonov.springcourse.service.UserService;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var connectionPool = new ConnectionPool();
-        var userRepository = new UserRepository(connectionPool);
-        var companyRepository = new CompanyRepository(connectionPool);
 
-        UserService userService = new UserService(userRepository, companyRepository);
+        var container = new Container();
+//
+//        var connectionPool = new ConnectionPool();
+//        var userRepository = new UserRepository(connectionPool);
+//        var companyRepository = new CompanyRepository(connectionPool);
+//
+//        UserService userService = new UserService(userRepository, companyRepository);
+//
+//        var connectionPool = container.get(ConnectionPool.class);
+//        var userRepository = container.get(UserRepository.class);
+//        var companyRepository = container.get(CompanyRepository.class);
+
+        var userService = container.get(UserService.class);
 
         //// TODO: 26.08.2023 userService
     }
---
+++ src/main/java/ru/philimonov/springcourse/ioc/Container.java
@@ -0,0 +1,7 @@
+package ru.philimonov.springcourse.ioc;
+
+public class Container {
+    public <T> T get(Class<T> clazz) {
+        return null;
+    }
+}
